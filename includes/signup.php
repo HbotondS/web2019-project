1b175bf4bd7e7eb8cc58ec0620c8f5cda1b5c98b
Botond Sámuel Hegyi <hbotond97@gmail.com>
2019-06-19 21:05:17 +0300

    uj user letrehozasa

--- includes/signup.php
+++ includes/signup.php
@@ -1,21 +1,32 @@
 <?php
     if (isset($_POST['submit'])) {
-        include_once 'connectUser.php';
+        include_once 'connectUsers.php';
 
         $first = $_POST['first'];
         $last = $_POST['last'];
-        $eamil = $_POST['email'];
+        $email = $_POST['email'];
         $uid = $_POST['uid'];
         $pwd = $_POST['pwd'];
 
-        if (empty($first) || empty($last) || empty($eamil) || empty($uid) || empty($pwd)) {
+        if (empty($first) || empty($last) || empty($email) || empty($uid) || empty($pwd)) {
             header("Location: ../user/index.php?signup=empty");
         } else {
             if (!preg_match("/^[a-zA-Z]*$/", $first) || !preg_match("/^[a-zA-Z]*$/", $last)) {
                 header("Location: ../user/index.php?signup=char");
-            } elseif (!filter_var($eamil, FILTER_VALIDATE_EMAIL)) {
+            } elseif (!filter_var($email, FILTER_VALIDATE_EMAIL)) {
                 header("Location: ../user/index.php?signup=invalidemail&first=$first&last=$last&uid=$uid");
             } else {
+                $first = mysqli_real_escape_string($conn, $first);
+                $last = mysqli_real_escape_string($conn, $last);
+                $email = mysqli_real_escape_string($conn, $email);
+                $uid = mysqli_real_escape_string($conn, $uid);
+                $pwd = mysqli_real_escape_string($conn, $pwd);
+                $pwdHashed = md5($pwd);
+
+                $sql = "insert into users (firsname, lastname, email, username, password) 
+                        values ('$first', '$last', '$email', '$uid', '$pwdHashed');";
+                mysqli_query($conn, $sql);
+
                 header("Location: ../user/index.php?signup=success");
             }
         }
