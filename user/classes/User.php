1a7875cb321108125d75dff75c2d9e5092dd5c68
Botond Sámuel Hegyi <hbotond97@gmail.com>
2019-06-20 03:06:42 +0300

    connect user & DPO

--- user/classes/User.php
+++ user/classes/User.php
@@ -1,18 +1,29 @@
 <?php
 
     class User {
+        // adatbazis kapcsolat
+        private $db;
+
+        private $id;
         private $firstname;
         private $lastname;
         private $email;
         private $uid;
         private $pwd;
 
-        public function __construct($firstname, $lastname, $email, $uid, $pwd) {
-            $this->firstname = $firstname;
-            $this->lastname = $lastname;
-            $this->email = $email;
-            $this->uid = $uid;
-            $this->pwd = $pwd;
+        public function __construct($id = 0, $firstname = '', $lastname = '', $email = '', $uid = '', $pwd = '') {
+            $this->db = (new MyPDO())->connect();
+            $this->id = $id;
+
+            if ($this->id) {
+                $this->getDataById($this->id);
+            } else {
+                $this->firstname = $firstname;
+                $this->lastname = $lastname;
+                $this->email = $email;
+                $this->uid = $uid;
+                $this->pwd = md5($pwd);
+            }
         }
 
         public function getFirstname() {
@@ -54,5 +65,52 @@
         public function setPwd($pwd) {
             $this->pwd = $pwd;
         }
+
+        // kiolvassa az adatokat ha ismert az id
+        private function getDataById($id) {
+            $idsql = filter_var($id, FILTER_VALIDATE_INT);
+            if ($idsql === false) {
+                return false;
+            } else {
+                $sql = "SELECT * FROM users WHERE id = $id";
+
+                $this->getData($sql);
+            }
+        }
+
+        // egy lekerdezes alapjan lekeri egy felhasznalo adatait
+        private function getData($sql) {
+            $stmt = $this->db->query($sql);
+            $a = $stmt->fetch(PDO::FETCH_ASSOC);
+
+            if ($a) {
+                if(!isset( $a['id'])){
+                    throw new Exception("Adatbázis hiba, hiányzó mező");
+                }
+                $this->id = $a['id'];
+                $this->firstname = $a['firstname'];
+                $this->lastname = $a['lastname'];
+                $this->email = $a['email'];
+                $this->uid = $a['username'];
+                $this->pwd = $a['password'];
+
+                return true;
+            } else {
+                // nincs ilyen felhasznalo
+                return false;
+            }
+        }
+
+        public function __toString() {
+            $s = '------------------------<br>';
+            $s .= "Id: " . $this->id . '<br>' .
+                "Firstname: " . $this->firstname . "<br>" .
+                "Lastname: " . $this->lastname . "<br>" .
+                "Email: " . $this->email . "<br>" .
+                "Uid: " . $this->uid;
+            $s .= '<br>------------------------<br>';
+            return $s;
+        }
+
 
     }
