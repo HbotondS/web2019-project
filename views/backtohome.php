4c5c2aab6292255c757d340aebd31c08dd162d7a
Botond Sámuel Hegyi <hbotond97@gmail.com>
2019-06-22 12:13:12 +0300

    fix issue #6

--- views/backtohome.php
+++ views/backtohome.php
@@ -1,2 +1,3 @@
 <link href="../styles/error.css" rel="stylesheet">
-<div style="float: right"><a href="start_view.php"><button>Vissza a kezdo lapra</button></a></div>
+<link href="../styles/signup.css" rel="stylesheet">
+<div style="float: right"><a href="start_view.php"><button>Vissza a kezdő lapra</button></a></div>
