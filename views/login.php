080a50ee3d8367f03f0428713b7315658d78ef27
Botond Sámuel Hegyi <hbotond97@gmail.com>
2019-06-22 13:36:12 +0300

    fix issue #8

--- views/login.php
+++ views/login.php
@@ -2,19 +2,25 @@
     include_once 'backtohome.php';
 ?>
 
-<div>
+<div class="my-container">
     <?php if (isset($_GET['error']) && $_GET['error'] == 'loginfailed'): ?>
         <p class="error">Hibas felhasznalonev vagy jelszo</p>
     <?php endif; ?>
     <h3>Belepes</h3>
     <form id="myform" action="../moduls/login.php" method="post">
-        <label for="uid">Felhasznalo nev</label>
-        <input id="uid" type="text" name="uid" maxlength="100" size="30" required>
+        <div class="row">
+            <label for="uid">Felhasznalo nev</label>
+            <input id="uid" type="text" name="uid" maxlength="100" size="30" required>
+        </div>
 
-        <label for="pwd">Jelszo</label>
-        <input id="pwd" type="password" name="pwd" size="30" required>
+        <div class="row">
+            <label for="pwd">Jelszo</label>
+            <input id="pwd" type="password" name="pwd" size="30" required>
+        </div>
 
-        <button type="submit" name="login-submit">Belepes</button>
-        <button type="reset" name="reset">Elfelejtett jelszo</button>
+        <div class="button-wrapper">
+            <button type="submit" name="login-submit">Belepes</button>
+            <button type="reset" name="reset">Elfelejtett jelszo</button>
+        </div>
     </form>
 </div>
