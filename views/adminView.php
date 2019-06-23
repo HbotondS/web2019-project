5c4fa12d43906af4d6d746fba87300635a6944bb
Botond Sámuel Hegyi <hbotond97@gmail.com>
2019-06-23 17:27:56 +0300

    admin feladatainak bovitese

--- views/adminView.php
+++ views/adminView.php
@@ -1,28 +1,5 @@
 <?php
-    include_once '../includes/autoload.php';
-    include_once '../moduls/userModel.php';
     require 'header.php';
-
-    $users = (new MyPDO())->getAllUser();
 ?>
 
-<h3>Az összes felhasználó</h3>
-<table>
-    <thead>
-    <tr>
-        <th>Név</th>
-        <th>E-mail</th>
-        <th>Felhasználónév</th>
-    </tr>
-    </thead>
-    <tbody>
-    <?php foreach ($users as $user): ?>
-        <tr>
-            <td><?= $user->getName() ?></td>
-            <td><?= $user->getEmail() ?></td>
-            <td><?= $user->getUsername() ?></td>
-            <td><a target="_self" href="adminUserView.php?id=<?=$user->getId()?>"><button>Megnyitás</button></a></td>
-        </tr>
-    <?php endforeach; ?>
-    </tbody>
-</table>
+<a target="_self" href="users.php"><button>Felhasználók</button></a>
---
+++ views/users.php
@@ -0,0 +1,29 @@
+<?php
+    include_once '../includes/autoload.php';
+    include_once '../moduls/userModel.php';
+    require 'header.php';
+
+    $users = (new MyPDO())->getAllUser();
+?>
+
+<a target="_self" href="adminView.php"><button type="submit" name="cancel">Vissza</button></a>
+<h3>Az összes felhasználó</h3>
+<table>
+    <thead>
+    <tr>
+        <th>Név</th>
+        <th>E-mail</th>
+        <th>Felhasználónév</th>
+    </tr>
+    </thead>
+    <tbody>
+    <?php foreach ($users as $user): ?>
+        <tr>
+            <td><?= $user->getName() ?></td>
+            <td><?= $user->getEmail() ?></td>
+            <td><?= $user->getUsername() ?></td>
+            <td><a target="_self" href="adminUserView.php?id=<?=$user->getId()?>"><button>Megnyitás</button></a></td>
+        </tr>
+    <?php endforeach; ?>
+    </tbody>
+</table>
